d74c5e3921fd209356c6c79dd4497defb80f72da
SergioPC <sergiolb@hotmail.es>
2024-05-09 15:29:42 -0500

    Quinto commit
    Se agrego el archivo de rutas de los articulos y su primer endpoint

---
+++ src/routes/articulo.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router(); //manejador de rutas de express
+const ArticuloSchema = require("../models/articulo");
+
+//Admin (falta clave) 
+router.post("/capitulos", (req, res) => {
+    const articulo = ArticuloSchema(req.body);
+    articulo
+        .save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+module.exports = router;
