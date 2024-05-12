04353b96b6ccb45997681ced52739527a71e9b3b
SergioPC <sergiolb@hotmail.es>
2024-05-12 14:24:36 -0500

    Decimo octavo commit
    Se agregaron dos peticiones para el usuario en donde busca los ejemplos por su titulo o por el id del articulo al que pertenece

--- src/routes/ejemplo.js
+++ src/routes/ejemplo.js
@@ -1,15 +1,31 @@
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
-const ArticuloSchema = require("../models/ejemplo");
+const ejemploSchema = require("../models/ejemplo");
 const verifyToken = require('./validate_token');
 
 //Admin (falta clave) 
 router.post("/ejemplos", verifyToken, (req, res) => {
-    const articulo = ArticuloSchema(req.body);
-    articulo
+    const ejemplo = ejemploSchema(req.body);
+    ejemplo
         .save()
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
+});
+
+//peticion del usuario
+
+//Buscar un ejemplo por titulo
+router.get("/ejemplo/:titulo",(req, res) => {
+    ArticuloSchema.findOne({ titulo: req.params.titulo })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }))
+});
+
+//Buscar un ejemplo por idArticulo al que pertenece
+router.get("/ejemplo/:idArticulo",(req, res) => {
+    ArticuloSchema.findOne({ idArticulo: req.params.idArticulo })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }))
 });
 
 module.exports = router;
