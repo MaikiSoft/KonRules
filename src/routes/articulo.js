6e24d51b67ef917271f3a0724207face12cecd30
SergioPC <sergiolb@hotmail.es>
2024-05-12 12:47:44 -0500

    Decimo quinto commit
    Se agregaron tres endPoint de búsqueda, dos de artículos y uno de capítulos

--- src/routes/articulo.js
+++ src/routes/articulo.js
@@ -3,7 +3,7 @@ const verifyToken = require('./validate_token');
 const router = express.Router(); //manejador de rutas de express
 const ArticuloSchema = require("../models/articulo");
 
-//Admin (falta clave) 
+//Admin
 router.post("/articulos", verifyToken, (req, res) => {
     const articulo = ArticuloSchema(req.body);
     articulo
@@ -12,14 +12,30 @@ router.post("/articulos", verifyToken, (req, res) => {
         .catch((error) => res.json({ message: error }));
 });
 
-//Elimina el articulo buscado por el ID (admin)
+//Elimina el articulo buscado por el ID
 router.delete("/articulo/delete/:id", verifyToken, (req, res) => {
     ArticuloSchema.findByIdAndDelete(req.params.id, req.body, { new: true })
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }))
 });
 
-//Buscar todos los articulos(Usuario)
+//Peticiones del usuario
+
+// Buscar un artículo por su título
+router.get("/articulo/:titulo",(req, res) => {
+    ArticuloSchema.findOne({ titulo: req.params.titulo })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }))
+});
+
+// Buscar un artículo por su número de artículo
+router.get("/articulo/numero/:numeroArticulo", (req, res) => {
+    ArticuloSchema.findOne({ numeroArticulo: req.params.numeroArticulo })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }))
+});
+
+//Buscar todos los articulos
 router.get("/articulos", (req, res) => {
     ArticuloSchema.find()
         .then((data) => res.json(data))
--- src/routes/capitulo.js
+++ src/routes/capitulo.js
@@ -12,10 +12,20 @@ router.post("/capitulos", verifyToken, (req, res) => {
         .catch((error) => res.json({ message: error }));
 });
 
-//Usuario
+//Peticiones del usuario
+
+//Busca todos los capitulos con sus respectivos articulos
 router.get("/capitulos", (req, res) => {
     capituloSchema.find().populate('articulos')
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
+
+//Busca todos los capitulos por palabra clave
+router.get("/capitulos/buscar/:palabraClave", verifyToken, (req, res) => {
+    capituloSchema.find({ palabrasClave: { $in: [req.params.palabraClave] } }).populate('articulos')
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }))
+});
+
 module.exports = router;
